9f008a1722713951e0997142a0a08383f19233ff
denisburlacu <burlacudenys@yahoo.com>
2022-02-03 21:25:11 +0200

    Added Iis support

---
+++ README.md
@@ -0,0 +1,2 @@
+# Yota.Hosts
+ 
---
+++ src/Yota.Iis/IisConfigurator.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Yota.MvcHelpers.Iis
+{
+    public static class IisConfigurator
+    {
+        public static void AddIis(this IServiceCollection services)
+        {
+            //This is required for RestSharp as he have some issues related to this
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            services.Configure<IISServerOptions>(options =>
+                                                 {
+                                                     options.AutomaticAuthentication = false;
+                                                 });
+            services.Configure<IISOptions>(options =>
+                                           {
+                                               options.ForwardClientCertificate = false;
+                                               options.AutomaticAuthentication = false;
+                                           });
+        }
+    }
+}
